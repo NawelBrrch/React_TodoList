aa98900cdb5d4a46caa223bb7ad6914c5f9e23bd
Nawel Berrichi <nawel.berrichi@hetic.net>
2020-06-29 17:40:49 +0200

    :tada: first commit

---
+++ src/components/Todos.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+
+
+class Todos extends React.Component {
+
+
+  render() {
+    return this.props.todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} toggleComplete={this.props.toggleComplete} delTodo={this.props.delTodo}/>
+    ));
+  }
+}
+
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+}
+
+export default Todos;
