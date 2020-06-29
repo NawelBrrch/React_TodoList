9f75bce65f9501f6810e7dd454e2f625d7ae938a
Nawel Berrichi <nawel.berrichi@hetic.net>
2020-06-29 18:53:30 +0200

    :rocket: add input

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
-import './App.css';
+import AddTodo from './components/AddTodo';
+import Header from './components/layout/Header';
+import { v4 as uuidv4 } from 'uuid';
+
+import './App.scss';
 
 class App extends Component {
   state = {
     todos:[
       {
-        id: 1,
+        id: uuidv4(),
         title: 'One',
         completed: false
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: 'Two',
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: 'Three',
         completed: false
       },
@@ -37,10 +41,24 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
   }
 
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} delTodo={this.delTodo}/>
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} delTodo={this.delTodo}/>
+        </div>
       </div>
     )
   }
